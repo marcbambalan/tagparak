79818db09b86a1fc683d077e46f6e903eba046be
Marc Bambalan <marcbambi@gmail.com>
2024-05-30 22:10:57 +0800

    Integrate signOut
    [Changes]
    - Sign out user on click of the sign out button
    - Convert menu to popover
    
    CU: https://app.clickup.com/t/86cvgde2m

--- src/app/ui/DashboardHeader.tsx
+++ src/app/ui/DashboardHeader.tsx
@@ -1,13 +1,28 @@
-import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+"use client";
+
+import {
+  Button,
+  Popover,
+  PopoverButton,
+  PopoverPanel,
+} from "@headlessui/react";
+import { UserCircleIcon } from "@heroicons/react/16/solid";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect } from "react";
+import { useFormState } from "react-dom";
+import { logout } from "@/app/lib/actions";
 
 export default function DashboardHeader() {
+  const [errorMessage, dispatch] = useFormState(logout, undefined);
+
+  useEffect(() => console.error(errorMessage), [errorMessage]);
+
   return (
     <div className="absolute z-10 w-full bg-white drop-shadow-lg">
       <div className="mx-auto flex max-w-screen-2xl items-center space-x-4 px-0 md:px-16 2xl:px-0">
         <div className="flex grow">
-          <a href="/dashboard" className="flex items-center">
+          <a href="/scheduler" className="flex items-center">
             <Image
               src="/images/tagparak-icon.png"
               alt="Tagparak Beachfront Resort"
@@ -17,45 +32,32 @@ export default function DashboardHeader() {
             <span className="hidden sm:block">Tagparak Dashboard</span>
           </a>
         </div>
-        <Menu>
-          <MenuButton>
-            <div className="relative mr-2 h-9 w-9 overflow-hidden rounded-full bg-yellow-100 md:mr-0">
-              <svg
-                className="absolute -left-1 h-11 w-11 text-yellow-400"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
+        <Popover className="relative flex">
+          <PopoverButton>
+            <div className="mr-2 h-9 w-9 rounded-full text-yellow-300 md:mr-0">
+              <UserCircleIcon />
             </div>
-          </MenuButton>
-          <MenuItems
+          </PopoverButton>
+          <PopoverPanel
             anchor={{ to: "bottom end", gap: 16 }}
             className="z-10 w-[160px] rounded bg-white p-3 text-center shadow-md"
           >
-            <MenuItem>
-              <Link
-                className="mb-2 block rounded-full px-4 py-2 data-[focus]:bg-yellow-100"
-                href="/profile"
+            <Link
+              className="mb-2 block rounded-full px-4 py-2 hover:bg-yellow-100"
+              href="/profile"
+            >
+              Profile
+            </Link>
+            <form action={dispatch}>
+              <Button
+                type="submit"
+                className="block w-full rounded-full px-4 py-2 hover:bg-red-600 hover:text-white"
               >
-                Profile
-              </Link>
-            </MenuItem>
-            <MenuItem>
-              <Link
-                className="block rounded-full px-4 py-2 data-[focus]:bg-red-600 data-[focus]:text-white"
-                href="/logout"
-              >
-                Logout
-              </Link>
-            </MenuItem>
-          </MenuItems>
-        </Menu>
+                Sign Out
+              </Button>
+            </form>
+          </PopoverPanel>
+        </Popover>
       </div>
     </div>
   );
