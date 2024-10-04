4072754bd3aeb75640856994fae774c21cc7046c
Marc Bambalan <marc@lexagle.com>
2024-10-04 22:56:00 +0800

    Add content to the Rentals page
    [Changes]
    - Create the RentalsTileView component
    - Add assets for the rental page
    
    CU: https://app.clickup.com/t/86cvdxzqk

--- src/app/(public)/rentals/page.tsx
+++ src/app/(public)/rentals/page.tsx
@@ -1,10 +1,13 @@
-import SectionHeader from '@/app/ui/SectionHeader';
+import { fetchRentals } from "@/app/lib/actions";
+import RentalsTileView from "@/app/ui/RentalsTileView";
+import SectionHeader from "@/app/ui/SectionHeader";
 
-export default function Page() {
+export default async function Page() {
+  const rentals = await fetchRentals();
   return (
     <div className="mx-auto h-full max-w-screen-2xl px-0 py-8 md:px-16 2xl:px-0">
       <SectionHeader text="rentals" />
-      {/* Insert page content */}
+      <RentalsTileView rentals={rentals} />
     </div>
   );
 }
