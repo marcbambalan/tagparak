bb553016b20596d7d1035ef532e2c63f462a721a
Marc Bambalan <marc@lexagle.com>
2024-10-04 18:51:29 +0800

    Fix: Wrong Google maps height

--- src/app/ui/VisitUs.tsx
+++ src/app/ui/VisitUs.tsx
@@ -3,15 +3,14 @@ import SectionHeader from "./SectionHeader";
 const VisitUs = ({ mapHeight }: { mapHeight: number }) => {
   return (
     <section className="mx-auto grid max-w-screen-2xl sm:grid-cols-1 sm:grid-rows-2 md:grid-cols-2 md:grid-rows-1 md:py-8 xl:px-0">
-      <div className={`h-[${mapHeight}px] w-full bg-slate-300`}>
-        <iframe
-          className="h-full w-full"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1223.2274716144923!2d122.64063081351568!3d13.84059842812784!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33a26de2fa516893%3A0xc0e7107776841113!2sTagparak%20Beachfront%20Resort!5e1!3m2!1sen!2sph!4v1728035655758!5m2!1sen!2sph"
-          allowFullScreen
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-        ></iframe>
-      </div>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1223.2274716144923!2d122.64063081351568!3d13.84059842812784!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33a26de2fa516893%3A0xc0e7107776841113!2sTagparak%20Beachfront%20Resort!5e1!3m2!1sen!2sph!4v1728035655758!5m2!1sen!2sph"
+        height={mapHeight}
+        width="100%"
+        allowFullScreen
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
       <div
         id="visit-us"
         className="order-first flex flex-col items-center justify-center md:order-last"
