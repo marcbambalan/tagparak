efc007c43fec911ef385a3f7ced438f54f514a62
Marc Bambalan <marcbambi@gmail.com>
2025-01-07 11:22:11 +0800

    Use eager loading on hero images
    [Changes]
    - Set the loading attribute of hero images in the landing and
    login pages to eager
    - Sort imports in the landing page
    
    CU: https://app.clickup.com/t/86cxk7ezd

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -19,6 +19,7 @@ export default function Page() {
             alt="Tagparak Beachfront Resort"
             fill
             className="object-cover"
+            loading="eager"
           />
         </div>
       </div>
